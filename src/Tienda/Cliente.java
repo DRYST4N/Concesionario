159b576bde4a093ab86cd1e23bb224f0b64903ec
DRYST4N <155493592+DRYST4N@users.noreply.github.com>
2025-01-13 13:50:48 +0100

    Update Cliente.java
    Documentacion Javadoc Clientes

--- src/Tienda/Cliente.java
+++ src/Tienda/Cliente.java
@@ -13,15 +13,49 @@ public class Cliente implements Serializable {
         this.Telefono = Telefono;
         this.Info = Info;
     }
+
+    /**
+     * Obtiene el identificador único del objeto.
+     * 
+     * Este método devuelve el valor de la variable {@code ID}, que representa 
+     * el identificador único asociado al objeto.
+     * 
+     * @return una cadena que contiene el identificador único del objeto.
+     */
     public String getID(){
         return ID;
     }
+    
+    /**
+     * Muestra la información detallada del objeto.
+     * 
+     * Este método imprime en la consola los valores de los atributos del objeto, 
+     * incluyendo el identificador único, el nombre, el teléfono y la preferencia de información.
+     * 
+     * Atributos mostrados:
+     * <ul>
+     *   <li>ID: Identificador único del objeto.</li>
+     *   <li>Nombre: Nombre asociado al objeto.</li>
+     *   <li>Telefono: Número de teléfono asociado al objeto.</li>
+     *   <li>Info: Preferencia sobre recibir información.</li>
+     * </ul>
+     */
     public void MostrarInfo(){
         System.out.println("ID: " + ID);
         System.out.println("Nombre: " + Nombre);
         System.out.println("Telefono: " + Telefono);
         System.out.println("Info: " + Info);
     }
+
+    /**
+     * Obtiene la preferencia de información asociada al objeto.
+     * 
+     * Este método devuelve el valor de la variable {@code Info}, que indica si 
+     * el objeto está configurado para recibir información.
+     * 
+     * @return {@code true} si el objeto está configurado para recibir información, 
+     *         {@code false} en caso contrario.
+     */
     public boolean getInfo(){
         return Info;
     }
